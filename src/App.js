a304eaa9359423042e12adff7df173eca0bd5ada
nbko <nbko@uchicago.edu>
2024-06-04 10:29:34 +0900

    restoring the frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Button from "@mui/material/Button";
+import { SearchInput, QuarterTags } from "./SearchInput";
+import DisabledAccordion from "./CourseReviews";
+import Progress from "./Progress";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import Alert from "@mui/material/Alert";
+import { majors, instructors } from "./data";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const themeOptions = createTheme({
+		palette: {
+			primary: {
+				main: "#543310",
+			},
+			secondary: {
+				main: "#AF8F6F",
+			},
+		},
+	});
+
+	const [progress, setProgress] = useState(false);
+	const [search, setSearch] = useState(false);
+	const [isClicked, setIsClicked] = useState(false);
+	const [reviewList, setReviewList] = useState([]);
+	const [major, setMajor] = useState(null);
+	const [instructor, setInstructor] = useState(null);
+	const [alertMsg, setAlertMsg] = useState("");
+
+	const handleSearch = () => {
+		setIsClicked(true);
+		if (!major) {
+			setAlertMsg("Please select a subject.");
+		} else if (!instructor) {
+			setAlertMsg("Please enter an instructor name.");
+		} else {
+			if (major && instructor) {
+				setSearch(true);
+				setProgress(true);
+			}
+		}
+	};
+
+	return (
+		<ThemeProvider theme={themeOptions}>
+			<div className="search-container">
+				<h2>Search Course Feedback for 2024-2025</h2>
+				<div className="search-content">
+					<SearchInput
+						inputType="major"
+						options={majors}
+						setSelectedMajor={setMajor}
+					/>
+					<SearchInput
+						inputType="instructor name"
+						options={instructors}
+						setSelectedMajor={setInstructor}
+					/>
+					<Button variant="contained" onClick={handleSearch}>
+						Search
+					</Button>
+					<div>Selected Major: {major ? major.label : "None"}</div>
+					<div>
+						Selected Instructor: {instructor ? instructor.label : "None"}
+					</div>
+					{isClicked && (!major || !instructor) && (
+						<Alert severity="warning">{alertMsg}</Alert>
+					)}
+					{progress && <Progress />}
+					<DisabledAccordion />
+					{/* <QuarterTags />
+				<SearchInput inputType="course section" /> */}
+				</div>
+			</div>
+		</ThemeProvider>
+	);
 }
 
 export default App;
