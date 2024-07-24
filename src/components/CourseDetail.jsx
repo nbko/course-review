a22c801d929a4a8d227aadfe48fcebbca48d5659
nbko <nbko@uchicago.edu>
2024-07-24 17:22:41 +0700

    add and style wireframe elements using mui

--- src/components/CourseDetail.jsx
+++ src/components/CourseDetail.jsx
@@ -7,6 +7,112 @@ import Paper from "@mui/material/Paper";
 import { useAtomValue } from "jotai";
 import * as post from "../state/atoms.js";
 
+import { styled, alpha } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+
+const StyledMenu = styled((props) => (
+	<Menu
+		elevation={0}
+		anchorOrigin={{
+			vertical: "bottom",
+			horizontal: "right",
+		}}
+		transformOrigin={{
+			vertical: "top",
+			horizontal: "right",
+		}}
+		{...props}
+	/>
+))(({ theme }) => ({
+	"& .MuiPaper-root": {
+		borderRadius: 6,
+		marginTop: theme.spacing(1),
+		minWidth: 180,
+		color:
+			theme.palette.mode === "light"
+				? "rgb(55, 65, 81)"
+				: theme.palette.grey[300],
+		boxShadow:
+			"rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px",
+		"& .MuiMenu-list": {
+			padding: "4px 0",
+		},
+		"& .MuiMenuItem-root": {
+			"& .MuiSvgIcon-root": {
+				fontSize: 18,
+				color: theme.palette.text.secondary,
+				marginRight: theme.spacing(1.5),
+			},
+			"&:active": {
+				backgroundColor: alpha(
+					theme.palette.primary.main,
+					theme.palette.action.selectedOpacity
+				),
+			},
+		},
+	},
+}));
+
+function ReviewFilter() {
+	const [anchorEl, setAnchorEl] = useState(null);
+	const open = Boolean(anchorEl);
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+
+	return (
+		<>
+			<Button
+				id="demo-customized-button"
+				aria-controls={open ? "review-filter" : undefined}
+				aria-haspopup="true"
+				aria-expanded={open ? "true" : undefined}
+				variant="contained"
+				disableElevation
+				onClick={handleClick}
+				endIcon={open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+				sx={{
+					backgroundColor: "#900000",
+
+					"&:hover": {
+						backgroundColor: "#900000",
+					},
+				}}
+			>
+				Overall
+			</Button>
+			<StyledMenu
+				id="review-filter"
+				MenuListProps={{
+					"aria-labelledby": "review-filter",
+				}}
+				anchorEl={anchorEl}
+				open={open}
+				onClose={handleClose}
+			>
+				<MenuItem onClick={handleClose} disableRipple>
+					Most Recent
+				</MenuItem>
+				<MenuItem onClick={handleClose} disableRipple>
+					Least Recent
+				</MenuItem>
+				<MenuItem onClick={handleClose} disableRipple>
+					Overall
+				</MenuItem>
+			</StyledMenu>
+		</>
+	);
+}
+
 // 수업 디테일 불러오는 함수
 function CourseDetail() {
 	//const courseSection = useAtomValue(post.courseSection);
@@ -19,16 +125,24 @@ function CourseDetail() {
 
 	useEffect(() => {
 		async function fetchCourseData() {
-			const summary = await getSummarizedReview(instructor, courseSection);
-
-			setCourseReviews(summary);
+			console.log(
+				`fetching course data for prof ${professorName} for course ${courseSection}`
+			);
+			if (professorName && courseSection) {
+				const summary = await getSummarizedReview(professorName, courseSection);
+				setCourseReviews(summary);
+			}
 		}
 		fetchCourseData();
 	}, []);
 
 	return (
 		<>
-			<div className="course-review__container">
+			<Container
+				className="course-review__container"
+				maxWidth={false}
+				disableGutters
+			>
 				<Navbar />
 				{courseReviews.length > 0 && (
 					<HeaderBadge
@@ -39,90 +153,93 @@ function CourseDetail() {
 				)}
 				<ProfProfile badgeLabel={"Instructor"} instructorName={professorName} />
 
-				<div className="reviews__wrapper" style={{ padding: "0 3rem" }}>
+				<Container className="reviews__wrapper" maxWidth="xl">
 					{console.log("course reviews:", courseReviews)}
-					<div className="course-review__box">
+					<Box className="course-review__box">
+						{courseReviews && (
+							<Box className="reviews-filter">
+								<div className="reviews-title">
+									Reviews
+									<span className="reviews-count">
+										{" "}
+										({courseReviews.length})
+									</span>
+								</div>
+								<div className="filter-right">
+									<ReviewFilter />
+									<span className="paginator">{"< 1 of 1>"}</span>
+								</div>
+							</Box>
+						)}
 						{courseReviews &&
 							courseReviews.map((course, i) => (
-								<div className="reviews_conatiner" key={i}>
-									<div className="reviews-filter">
-										<div className="reviews-title">
-											Reviews
-											<span className="reviews-count">
-												{" "}
-												({courseReviews.length})
-											</span>
+								<Paper
+									className="reviews_conatiner"
+									key={i}
+									sx={{
+										padding: "1rem 2rem",
+										flex: 2,
+										border: "0.01rem solid #d9d9d9",
+										boxShadow: "none",
+										mb: "2rem",
+									}}
+								>
+									<h4 className="course-info">
+										<div className="professor-name">
+											Professor {course.instructors.join(", ")}
 										</div>
-										<div className="filter-right">
-											<div className="sorting-dropdown">Overall</div>
-											<div className="paginator">{"< 1 of 1>"}</div>
-										</div>
+										<div className="quarter">Quarter: {course.semester}</div>
+									</h4>
+									<div className="course-reviews">
+										<p className="comments course">
+											<strong>{"Course Comments: "}</strong>
+											{course.comments_course}
+										</p>
+										<p className="comments professor">
+											<strong>{"Professor Comments: "}</strong>
+											{course.comments_professor}
+										</p>
+										<p className="comments course-content">
+											<strong>{"Course Content: "}</strong>
+											{course.course_content}
+										</p>
+										<p className="comments advice-section">
+											<strong>{"Advice: "}</strong>
+											{course.advice}
+										</p>
 									</div>
-									<Paper
-										style={{
-											padding: "1rem 2rem",
-											flex: 2,
-											border: "0.01rem solid #d9d9d9",
-											boxShadow: "none",
-										}}
-									>
-										<h4 className="course-info">
-											<div className="professor-name">
-												Professor {course.instructors.join(", ")}
-											</div>
-											<div className="quarter">Quarter: {course.semester}</div>
-										</h4>
-										<div className="course-reviews">
-											<p className="comments course">
-												<strong>{"Course Comments: "}</strong>
-												{course.comments_course}
-											</p>
-											<p className="comments professor">
-												<strong>{"Professor Comments: "}</strong>
-												{course.comments_professor}
-											</p>
-											<p className="comments course-content">
-												<strong>{"Course Content: "}</strong>
-												{course.course_content}
-											</p>
-											<p className="comments advice-section">
-												<strong>{"Advice: "}</strong>
-												{course.advice}
-											</p>
-										</div>
-									</Paper>
-								</div>
+								</Paper>
 							))}
-						<div
-							className="all-ratings"
+					</Box>
+					<Box
+						className="all-ratings"
+						style={{
+							display: "flex",
+							flexDirection: "column",
+							marginLeft: "2rem",
+							flex: 1,
+						}}
+					>
+						<Paper
 							style={{
-								display: "flex",
-								flexDirection: "column",
-								marginLeft: "2rem",
-								flex: 1,
+								padding: "2rem",
+								height: "45%",
+								border: "0.01rem solid #d9d9d9",
+								boxShadow: "none",
 							}}
-						>
-							<Paper
-								style={{
-									padding: "2rem",
-									height: "45%",
-									border: "0.01rem solid #d9d9d9",
-									boxShadow: "none",
-								}}
-							></Paper>
-							<Paper
-								style={{
-									padding: "2rem",
-									height: "50%",
-									border: "0.01rem solid #d9d9d9",
-									marginTop: "2rem",
-									boxShadow: "none",
-								}}
-							></Paper>
-						</div>
-					</div>
-				</div>
-			</div>
+						></Paper>
+						<Paper
+							style={{
+								padding: "2rem",
+								height: "50%",
+								border: "0.01rem solid #d9d9d9",
+								marginTop: "2rem",
+								boxShadow: "none",
+							}}
+						></Paper>
+					</Box>
+				</Container>
+			</Container>
 		</>
 	);
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,135 @@
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import InputBase from "@mui/material/InputBase";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import SearchIcon from "@mui/icons-material/Search";
+
+const Search = styled("div")(({ theme }) => ({
+	position: "relative",
+	borderRadius: theme.shape.borderRadius,
+	backgroundColor: "#90000015",
+	"&:hover": {
+		backgroundColor: "#90000025",
+	},
+	marginRight: theme.spacing(2),
+	marginLeft: 0,
+	width: "100%",
+	[theme.breakpoints.up("sm")]: {
+		marginLeft: theme.spacing(3),
+		width: "auto",
+	},
+	padding: "0.25rem",
+	display: "flex",
+	alignItems: "center",
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+	padding: theme.spacing(0, 2),
+	height: "100%",
+	position: "absolute",
+	pointerEvents: "none",
+	display: "flex",
+	alignItems: "center",
+	justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+	color: "inherit",
+	"& .MuiInputBase-input": {
+		padding: theme.spacing(1, 1, 1, 0),
+		// vertical padding + font size from searchIcon
+		paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+		transition: theme.transitions.create("width"),
+		width: "100%",
+		[theme.breakpoints.up("md")]: {
+			width: "50ch",
+		},
+	},
+}));
+
 const Navbar = () => {
 	return (
-		<nav className="content-row">
-			<a href={`/`} label="logo" className="prof-nav__bar logo">
-				Maroon Review
-			</a>
-			<div className="prof-nav__bar search-bar">Search Bar</div>
-			<div className="prof-nav__bar login">Login</div>
-			<div className="prof-nav__bar sign-up">Sign Up</div>
-		</nav>
+		<AppBar
+			position="static"
+			sx={{ background: "#fff", color: "#900000", padding: "0.3rem" }}
+		>
+			<Container
+				maxWidth="xl"
+				sx={{
+					display: "flex",
+					alignItems: "center",
+					justifyContent: "center",
+				}}
+			>
+				<Toolbar disableGutters>
+					<Typography
+						variant="h6"
+						noWrap
+						component="a"
+						href="/"
+						sx={{
+							ml: 3,
+							display: { xs: "none", md: "flex" },
+							fontWeight: 700,
+							color: "inherit",
+							textDecoration: "none",
+							textTransform: "uppercase",
+						}}
+					>
+						Maroon Reviews
+					</Typography>
+					<Search>
+						<SearchIconWrapper>
+							<SearchIcon />
+						</SearchIconWrapper>
+						<StyledInputBase
+							placeholder="Search…"
+							inputProps={{ "aria-label": "search" }}
+						/>
+					</Search>
+					<Box
+						sx={{
+							flexGrow: 1,
+							display: { xs: "none", md: "flex" },
+						}}
+					>
+						<Button
+							sx={{
+								color: "#900000",
+								display: "block",
+								padding: ".6rem 2.5rem",
+								fontWeight: "700",
+								fontSize: "1rem",
+								marginRight: "1rem",
+								"&:hover": {
+									backgroundColor: "#fff",
+								},
+							}}
+						>
+							Login
+						</Button>
+						<Button
+							sx={{
+								color: "#900000",
+								display: "block",
+								padding: ".6rem 2.5rem",
+								fontWeight: "700",
+								fontSize: "1rem",
+								"&:hover": {
+									backgroundColor: "#fff",
+								},
+							}}
+						>
+							Sign Up
+						</Button>
+					</Box>
+				</Toolbar>
+			</Container>
+		</AppBar>
 	);
 };
 
